956b0cb29c8aa4bf167fc5603ee62ab3bc6b9e1a
Faiz_Nasir <126837890+FaizNasir00@users.noreply.github.com>
2023-05-30 16:33:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="/img/icon.png">
-    <title>Program Accreditation Information System</title>
+    <title>UiTM Malaysia Program Accreditation Information System</title>
     <link rel="stylesheet" href="./css/index2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
