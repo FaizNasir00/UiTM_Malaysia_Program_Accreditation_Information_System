2b976dbe4abd6904c1086c33a40c21ba91d2da65
Faiz_Nasir <126837890+FaizNasir00@users.noreply.github.com>
2023-05-30 16:37:59 +0800

    Update p_akreditasi.php

--- p_akreditasi.php
+++ p_akreditasi.php
@@ -223,10 +223,10 @@
 
 			<?php
 				// Database connection
-				$servername = "localhost";
-                $username = "inqkacom_faiz123";
-                $password = "faiznasir123";
-                $dbname = "inqkacom_pais";
+				$servername = "";
+				$username = "";
+				$password = "";
+				$dbname = "";
 				$conn = new mysqli($servername, $username, $password, $dbname);
 				if ($conn->connect_error) {
 					die("Connection failed: " . $conn->connect_error);
