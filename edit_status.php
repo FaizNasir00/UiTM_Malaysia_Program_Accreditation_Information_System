a17839f0fa76079351c5f22dae75eaeb6fa336aa
Faiz_Nasir <126837890+FaizNasir00@users.noreply.github.com>
2023-05-30 16:31:30 +0800

    Update edit_status.php

--- edit_status.php
+++ edit_status.php
@@ -159,7 +159,7 @@
 
 <?php
 // Database connection
-$conn = mysqli_connect("localhost", "inqkacom_faiz123", "faiznasir123", "inqkacom_pais");
+$conn = mysqli_connect("", "", "", "");
 
 // Check connection
 if (!$conn) {
